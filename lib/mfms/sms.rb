17d9e65abb8957a28e2458d5583b7edc45b9a804
Molchanov Andrey <neodelf@gmail.com>
2016-01-27 12:37:55 +0300

    added work with accounts

--- lib/mfms/sms.rb
+++ lib/mfms/sms.rb
@@ -7,37 +7,42 @@ require 'russian'
 module Mfms
   class SMS
 
-    attr_accessor :phone, :subject, :message
+    attr_accessor :phone, :subject, :message, :account, :login, :password, :server, :cert, :port, :ssl_port, :ssl
     attr_reader :id, :status
 
-    def initialize(phone, subject, message, translit = nil)
+    def initialize(phone, subject, message, translit = nil, account = nil)
       @phone = phone
       @subject = subject
       @message = message
       @status = 'not-sent'
-      @translit = translit.nil? ? @@translit : translit
-
+      account = "@@#{account}".to_sym
+      account_variable = if account.present? && self.class.class_variables.include?(account)
+                           account
+                         else
+                           self.class.class_variables.select{|sym| sym.to_s.include?('revoup0')}.first
+                         end
+      account_settings = Mfms::SMS.class_variable_get(account_variable)
+      @login = account_settings[:login]
+      @password = account_settings[:password]
+      @ssl = account_settings[:ssl]
+      @ssl_port = account_settings[:ssl_port]
+      @port = account_settings[:port]
+      @cert = account_settings[:cert]
+      @server = account_settings[:server]
+      @translit = translit.nil? ? account_settings[:translit] : translit
       validate!
     end
 
-    def self.settings=(settings={})
-      @@login = settings[:login]
-      @@password = settings[:password]
-      @@server = settings[:server]
-      @@port = settings[:port]
-      @@ssl_port = settings[:ssl_port]
-      @@cert_store = init_cert_store settings[:cert]
-      @@ssl = !settings[:ssl].nil? ? settings[:ssl] : true # connect using ssl by default
-      @@translit = !settings[:translit].nil? ? settings[:translit] : false # use translit or not
-      validate_settings!
-    end
-
-    def self.ssl=(flag)
-      @@ssl = flag
-    end
-
-    def self.ssl
-      @@ssl
+    def self.settings=(settings=[])
+      settings.each do |setting|
+        account = setting.keys.first
+        account_settings = setting[account]
+        account_settings[:cert] = init_cert_store(account_settings[:cert])
+        account_settings[:ssl] = account_settings[:ssl].presence || true
+        account_settings[:translit] = account_settings[:translit].presence || false
+        class_variable_set("@@#{account}", account_settings)
+        validate_settings!(account_settings)
+      end
     end
 
     # => SMS send status codes:
@@ -50,7 +55,7 @@ module Mfms
 
     def send
       #return stubbed_send if (defined?(Rails) && !Rails.env.production?)
-      self.class.establish_connection.start do |http|
+      establish_connection.start do |http|
         request = Net::HTTP::Get.new(send_url)
         response = http.request(request)
         body = response.body.split(';')
@@ -99,42 +104,42 @@ module Mfms
 
     private
 
-      def self.establish_connection
-        port = @@ssl ? @@ssl_port : @@port
-        http = Net::HTTP.new(@@server, port)
-        http.use_ssl = @@ssl
-        http.cert_store = @@cert_store
-        http
-      end
+    def establish_connection
+      port = @ssl ? @ssl_port : @port
+      http = Net::HTTP.new(@server, port)
+      http.use_ssl = @ssl
+      http.cert_store = @cert
+      http
+    end
 
-      def self.init_cert_store cert
-        raise ArgumentError, "Path to certificate should be defined for #{self}." if cert.nil?
-        raise ArgumentError, "Certificate file '#{File.expand_path(cert)}' does not exist." unless File.exist?(cert)
-        cert_store = OpenSSL::X509::Store.new
-        cert_store.add_cert OpenSSL::X509::Certificate.new File.read(cert)
-        cert_store
-      end
+    def self.init_cert_store(cert)
+      raise ArgumentError, "Path to certificate should be defined for #{self}." if cert.nil?
+      raise ArgumentError, "Certificate file '#{File.expand_path(cert)}' does not exist." unless File.exist?(cert)
+      cert_store = OpenSSL::X509::Store.new
+      cert_store.add_cert OpenSSL::X509::Certificate.new File.read(cert)
+      cert_store
+    end
 
-      def self.validate_settings!
-        raise ArgumentError, "Login should be defined for #{self}." if @@login.nil?
-        raise ArgumentError, "Password should be defined for #{self}." if @@password.nil?
-        raise ArgumentError, "Server should be defined for #{self}." if @@server.nil?
-        raise ArgumentError, "Port should be defined for #{self}." if @@port.nil?
-        raise ArgumentError, "Port for ssl should be defined for #{self}." if @@ssl_port.nil?
-        raise ArgumentError, "Port should contain only numbers. #{@@port.inspect} is given." unless @@port.instance_of?(Fixnum)
-        raise ArgumentError, "Port for ssl should contain only numbers. #{@@ssl_port.inspect} is given." unless @@ssl_port.instance_of?(Fixnum)
-      end
+    def self.validate_settings!(settings)
+      raise ArgumentError, "Login should be defined for #{self}." if settings[:login].nil?
+      raise ArgumentError, "Password should be defined for #{self}." if settings[:password].nil?
+      raise ArgumentError, "Server should be defined for #{self}." if settings[:server].nil?
+      raise ArgumentError, "Port should be defined for #{self}." if settings[:port].nil?
+      raise ArgumentError, "Port for ssl should be defined for #{self}." if settings[:ssl_port].nil?
+      raise ArgumentError, "Port should contain only numbers. #{settings[:port].inspect} is given." unless settings[:port].instance_of?(Fixnum)
+      raise ArgumentError, "Port for ssl should contain only numbers. #{settings[:ssl_port].inspect} is given." unless settings[:ssl_port].instance_of?(Fixnum)
+    end
 
-      def send_url
-        message = @translit ? Russian.translit(@message) : @message
-        "/revoup/connector0/send?login=#{@@login}&password=#{@@password}&" +
-        "subject[0]=#{@subject}&address[0]=#{@phone}&text[0]=#{URI.encode(message)}"
-      end
+    def send_url
+      message = @translit ? Russian.translit(@message) : @message
+      "/revoup/connector0/send?login=#{@login}&password=#{@password}&" +
+          "subject[0]=#{@subject}&address[0]=#{@phone}&text[0]=#{URI.encode(message)}"
+    end
 
-      def self.status_url msg_id
-        "/revoup/connector0/status?login=#{@@login}&password=#{@@password}&" +
-        "providerId[0]=#{msg_id}"
-      end
+    def self.status_url(msg_id)
+      "/revoup/connector0/status?login=#{@login}&password=#{@password}&" +
+          "providerId[0]=#{msg_id}"
+    end
 
   end
 end
--- lib/mfms/version.rb
+++ lib/mfms/version.rb
@@ -1,8 +1,8 @@
 module Mfms
   class Version
     MAJOR = 0
-    MINOR = 5
-    PATCH = 1
+    MINOR = 6
+    PATCH = 0
 
     def self.to_s
       "#{MAJOR}.#{MINOR}.#{PATCH}"
